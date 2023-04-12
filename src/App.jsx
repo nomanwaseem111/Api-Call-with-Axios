9f1ae0eb1e47dd56fa2ceb25e09f6ed49b078329
nomanwaseem111 <nomanwaseem111@gmail.com>
2023-04-12 18:38:28 +0500

    api call with axios

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useEffect,useState } from 'react'
+import ShoppingCart from './components/ShoppingCart'
+
+
+const App = () => {
+  
+  const [showData, setShowData] = useState([])
+
+  useEffect(() => {
+  
+
+        try {
+
+          const getData = async () => {
+         
+            const response = await axios.get('https://fakestoreapi.com/products')
+       
+            const data = await response.data
+       
+            setShowData(data);
+
+            console.log(data);
+
+                  
+            }
+            getData()
+          
+        } catch (error) {
+           console.log(error);
+        }
+
+  }, [])
+  
+  return (
+    <>
+      {
+        showData.length > 0 ? <ShoppingCart data={showData} /> : <h1>Loading....</h1> 
+      }
+
+    </>
+  )
+}
+
+export default App
